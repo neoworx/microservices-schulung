741873f9376be4449497bd3a1039966df4acdb8a
Patrick Hock <30664154+hawkpatrick@users.noreply.github.com>
2026-03-05 16:29:12 +0100

    Update FahrzeugControllerTest.java

--- fahrzeuge/src/test/java/de/hdpgmbh/fahrzeuge/FahrzeugControllerTest.java
+++ fahrzeuge/src/test/java/de/hdpgmbh/fahrzeuge/FahrzeugControllerTest.java
@@ -1,18 +1,21 @@
+
 package de.hdpgmbh.fahrzeuge;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.hasSize;
 
-import de.hdpgmbh.fahrzeuge.controller.FahrzeugController;
+import de.hdpgmbh.fahrzeuge.controller.v1.FahrzeugController;
 import de.hdpgmbh.fahrzeuge.model.Fahrzeug;
 import de.hdpgmbh.fahrzeuge.repository.FahrzeugRepository;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("h2")
 class FahrzeugControllerTest {
 
   @Autowired
@@ -20,6 +23,9 @@ class FahrzeugControllerTest {
 
   @Autowired
   private FahrzeugRepository fahrzeugRepository;
+
+  @Test
+  public void contextLoads() {}
 
   @Test
   void createFahrzeug_shouldCreateCorrectFahrzeug() {
@@ -38,5 +44,4 @@ class FahrzeugControllerTest {
   }
 
 }
-
 
